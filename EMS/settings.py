49cf911b096366824bf42f5d31f7392829a72ba0
pritsp29 <patelsprit2@gmail.com>
2024-08-20 10:55:14 -0400

    Changes to be committed: 	new file:   .dockerignore 	new file:   Dockerfile 	modified:   EMS/settings.py 	new file:   requirements.txt

--- EMS/settings.py
+++ EMS/settings.py
@@ -77,17 +77,27 @@ WSGI_APPLICATION = 'EMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'django', 
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django', 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1', 
+        'PASSWORD': 'mypassword',
+        'HOST': 'my-postgres',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
