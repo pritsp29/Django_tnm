2b007f23446efb7921efafdfa8222833fed5d896
pritsp29 <patelsprit2@gmail.com>
2024-08-21 10:13:45 -0400

    added all allowed host

--- EMS/settings.py
+++ EMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5n7%l=72+a(m=oo+ec1_kxd%^wp1$eo3xx3)6a6794*-t=#(ch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
